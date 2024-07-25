8d862f405bbeca5e900d3a088b5c20b60b874688
telescopelight <telescopelight>
2024-07-25 14:43:04 +0900

    Initial Commit

---
+++ Simultaneous_camera/Simultaneous_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simultaneous_cameraApp.swift
+//  Simultaneous_camera
+//
+//  Created by telescopelight on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Simultaneous_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
